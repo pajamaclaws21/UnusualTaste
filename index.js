c832fec81dd30235c84c8223024893ffee1d21a6
PJ <95423707+pajamaclaws21@users.noreply.github.com>
2022-10-27 14:53:56 -0500

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ app.get("/", function routeHandler(req, res){
            <p>A small CORS tool, because AllOrigins sometimes fails. You can use it to get by some basic annoyances and directly request other domains.</p>
            <br>
            <p>To go to a site, go to https://unusualtaste.onrender.com/view/YOUR_URL</p>
+           <br>
+           <p>Check out UnusualTaste's </p><a href="https://github.com/pajamaclaws21/UnusualTaste/">Github Repo</a><p>!</p>
   `);
 });
 
