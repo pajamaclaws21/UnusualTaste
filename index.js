a01d491fee78363be0e2863166e910b61cab9879
PJ <95423707+pajamaclaws21@users.noreply.github.com>
2022-10-27 17:57:36 +0000

    added link

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get("/", (req, res) =>
   res.send("<h1>UnusualTaste</h1>");
   res.send("<p>A small CORS tool, because AllOrigins sometimes fails.</p>")
   res.send("<p>You can use it to get by some basic annoyances and directly request other domains.</p>")
-  res.send("<p>To go to a site, go to https://")
+  res.send("<p>To go to a site, go to https://unusualtaste.onrender.com/view/YOUR_URL</p>")
 );
 
 // start the server and allow unblocker to proxy websockets:
