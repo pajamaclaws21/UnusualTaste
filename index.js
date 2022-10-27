da3e79adb1269191d1f320772b6c757b5a3c6897
PJ <95423707+pajamaclaws21@users.noreply.github.com>
2022-10-27 17:53:58 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const express = require("express");
+const Unblocker = require("unblocker");
+const app = express();
+const unblocker = Unblocker({
+  prefix: '/view/'
+});
+
+app.use(unblocker);
+
+app.get("/", (req, res) =>
+  res.send("<h1>UnusualTaste</h1>");
+  res.send("<p>A small CORS tool, because AllOrigins sometimes fails.</p>")
+  res.send("<p>You can use it to get by some basic annoyances and directly request other domains.</p>")
+  res.send("<p>To go to a site, go to https://")
+);
+
+// start the server and allow unblocker to proxy websockets:
+const port = process.env.PORT || 8080;
+app.listen(port).on("upgrade", unblocker.onUpgrade);
+console.log(`unblocker app live at http://localhost:${port}/`);
